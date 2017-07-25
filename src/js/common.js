bb74c6594a998e77b5676d2aab763afef7b51454
5jybu <jy522hi@outlook.kr>
2017-07-25 17:53:24 +0900

    7.25_

--- src/js/common.js
+++ src/js/common.js
@@ -9,3 +9,18 @@ $('.header-sub-menu2 > li').on('click', function () {       // header-sub-menu2 
 
     location.href = './newmenu.html';
 });
+
+$('.header-main-menu > li').on('mouseover', function() {
+    $(this).find(header-menu-text2).show();
+
+    /*header-menu-text2.slideToggle();*/
+});
+
+
+/*
+$('.header-menu-text').on('mouseout', function() {
+    $(this).attr('.header-sub-menu2').slideToggle();
+});
+*/
+
+
