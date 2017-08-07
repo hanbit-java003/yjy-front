ca3f6283f2b391ee3a5dcb49a6e8c1bdc08b35f0
5jybu <jy522hi@outlook.kr>
2017-08-07 17:54:55 +0900

    8.7

--- src/js/common.js
+++ src/js/common.js
@@ -22,42 +22,142 @@ $('.header-main-menu > li').on('mouseout', function() {
 
 
 
+var slides = [{
+    image: '../src/img/mainBanner_2.jpg'
+}, {
+    image: '../src/img/mainBanner_3.jpg'
+}, {
+    image: '../src/img/mainBanner_4.jpg'
+}];
+
+var carousel = $('.hc-activity-photos .hc-carousel-visible');
+var currentSlideIndex = 0;
+var animating = false;
+
+function slide(direction) {
+    animating = true;
+    clearTimeout(timer);
+
+    var currentSlide = slides[currentSlideIndex];
+    var nextSlide;
+
+    if (direction === left) {
+        nextSlide = slides[++currentSlideIndex];
+
+        if (!nextSlide) {
+            currentSlideIndex[currentSlideIndex];
+        }
+    }
+    else if (direction === 'right') {
+        nextSlide = slides[--currentSlideIndex];
+
+        if (!nextSlide) {
+            currentSlideIndex = slides.length;
+            nextSlide = slides[currentSlideIndex]
+        }
+    }
+
+    var currentElement = carousel.find('li');
+    var nextElement = $('<li></li>');
+    nextElement.css('background-image', nextSlide.image);
+    var animationLeft;
+
+    if (direction === 'left') {
+        nextElement.css('left', '100%');
+        animationLeft = '-=100%';
+    }
+    else if (direction === 'right') {
+        nextElement.css('left', '-100%');
+        animationLeft = '+=100%'
+    }
+
+    carousel.append(nextElement);
+
+    carousel.find('li').animate({
+        left: animationLeft
+    }, {
+        duration: 500,
+        complate: function () {
+            currentElement.remove();
+            animating = false;
+
+            if (this === currentElement[0]) {
+                return;
+            }
+
+            timer = setTimeout(function () {
+                slide('left');
+            }, 3000);
+        }
+    });
+}
+
+
+$('hc-carousel-circle').on('click', function () {
+   if (animating) {
+       return;
+   }
+
+   if ($(this).hasClass('left')) {
+       slide('left');
+   }
+   else if ($(this).hasClass('right')) {
+       slide('right');
+   }
+});
+
+
+var timer = setTimeout(function () {
+   slide('left');
+}, 2000);
 
 
 
 
 
-$('.hc-main-visual-slide').ready(function () {      //메인화면 사진 바뀌게
+
+
+
+
+
+
+
+
+
+
+
+
+
+//메인화면 이미지 변경 안됨ㅠㅠ
+/*var photo = ".jpg"; *///1dept 이미지 확장자
+
+/*$('.hc-main-slide').ready(function () {      //메인화면 사진 바뀌게
     $('.bs-slider').bsSlider({
         mode: 'fade',
         auto: true
     });
+});*/
+/*
+
+$('.hc-main-slide > li.img')
+    .mouseenter(function() {
+        var imgNm = $(this).find('img').attr('src').split(photo)[0];
+        $(this).find('img').attr('src', imgNm + 'active' + photo);
+
+        $('.hc-main-slide > li.img').find('img').stop().animate({'opacity':1}, 300);
+        $('.hc-main-slide_layer').slideDown(300);
+    })
+    .mouseleave( function() {
+        var imgNm = $(this).find('img').attr('src').split('_on')[0];
+        $(this).find('img').attr('src', imgNm + photo);
+    });
+
+$('.hc-main-slide_layer')
+    .mouseleave( function() {
+        $('.hc-main-slide > li.img').find('img').stop().animate({'opacity':0}, 100);
+        $('.hc-main-slide_layer').stop(true, true).slideUp(300);
+    });
+*/
 
 
-    var mainImgExt = ".jpg"; //1dept 이미지 확장자
-
-    $('.hc-main-visual-slide > li')
-        .mouseenter( function() {
-            var imgNm = $(this).find('img').attr('src').split(mainImgExt)[0]
-            $(this).find('img').attr('src', imgNm + '_on' + mainImgExt);
-
-            $('.hc-main-visual-slide > li').find('span').stop().animate({'opacity':1}, 300);
-            $('.hc-main-visual-slide_layer').slideDown(300);
-        })
-        .mouseleave( function() {
-            var imgNm = $(this).find('img').attr('src').split('_on')[0];
-            $(this).find('img').attr('src', imgNm + mainImgExt);
-        });
-
-    $(".hc-main-visual-slide_layer")
-        .mouseleave( function() {
-            $('.hc-main-visual-slide > li').find('span').stop().animate({'opacity':0}, 100);
-            $('.hc-main-visual-slide_layer').stop(true, true).slideUp(300);
-        });
-
-
-
-
-
-});
 
